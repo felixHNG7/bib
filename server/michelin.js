a591287e1c7e5fa98395504d720ca65c8dd24e58
felixHNG7 <felix.huang@edu.devinci.fr>
2020-03-16 12:33:59 +0100

    feat(michelin): get list of bib-gourmand restaurants

--- server/michelin.js
+++ server/michelin.js
@@ -2,34 +2,92 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 /**
- * Parse webpage restaurant
- * @param  {String} data - html response
- * @return {Object} restaurant
+ * Scrape each restaurants url
+ * @param  {String}  url
+ * @return {Object} urls
  */
-const parse = data => {
-  const $ = cheerio.load(data);
-  const name = $('.section-main h2.restaurant-details__heading--title').text();
-  const experience = $('#experience-section > ul > li:nth-child(2)').text();
+const scrapeUrls = async url =>{
+  var i=0;
+  const urls=[];
+  while(true){
+    i++;
+    const response = await axios(url+i);
+    const {data, status} = response;
+    const $ = cheerio.load(data);
 
-  return {name, experience};
-};
+
+    if (status >= 200 && status < 300) {
+      const restTable = $('.restaurant__list-row>div.col-md-6');
+      restTable.each(function(){
+        const url = $(this).find('.link ').attr('href');
+        urls.push(url);
+      });
+    }
+
+    let numbers = $('.js-restaurant__stats > h1').text();
+    numbers = numbers.replace(/\s/g, '');
+    first= numbers.substring(0,3);
+    last= numbers.substring(4,7);
+    if(first == last){ break;}
+
+
+  }
+  return urls;
+}
+/**
+ * parse Retaurants data
+ * @param  {Object}  data
+ * @return {Object} restaurants
+ */
+const parseRestaurant = data =>{
+  const $ = cheerio.load(data);
+  const image = $('.masthead__gallery-image-item').attr('data-image');
+  const name = $('.section-main h2.restaurant-details__heading--title').text();
+  const heading_list=[];
+  $('.restaurant-details__heading--list>li').each(function () {
+        heading_list.push($(this).text());
+    });
+  const localization = heading_list[0];
+  let [street,city,zipcode,country] = localization.split(",")
+  if(zipcode != undefined)
+  {zipcode = zipcode.trim();}
+  let experience = $('#experience-section > ul > li:nth-child(2)').text().trim();
+  if(experience!=undefined)
+  {experience = experience.substring(38);}
+  let phone = $('[data-event="CTA_tel"]').attr('href');
+  if(phone!= undefined){
+  phone = phone.substring(3);}
+  const link = $('[data-event="CTA_website"]').attr('href');
+  return {name,localization,experience,image,link,phone,zipcode};
+}
+
+
+const scrapeRest = async url =>{
+
+    const response = await axios(url);
+    const {data, status} = response;
+    if (status >= 200 && status < 300) {
+      return parseRestaurant(data);
+    }
+    console.error(status);
+    return null;
+
+}
 
 /**
  * Scrape a given restaurant url
  * @param  {String}  url
- * @return {Object} restaurant
+ * @return {Object} restaurants
  */
-module.exports.scrapeRestaurant = async url => {
-  const response = await axios(url);
-  const {data, status} = response;
-
-  if (status >= 200 && status < 300) {
-    return parse(data);
+module.exports.scrapeRestaurants = async url => {
+  const URLS =  await scrapeUrls(url);
+  let url_;
+  const restaurants = [];
+  for(let i=0;i<URLS.length;i++){
+    url_ = "https://guide.michelin.com"+URLS[i];
+    restaurants.push(await scrapeRest(url_));
   }
-
-  console.error(status);
-
-  return null;
+  return restaurants;
 };
 
 /**
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,14 +1,27 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
+const maitre = require('./maitre');
+'use strict';
+const fs = require('fs');
 
-async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche') {
+async function michelin_to_json (searchLink = 'https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/') {
   try {
-    console.log(`🕵️‍♀️  browsing ${searchLink} source`);
+    console.log(`browsing ${searchLink} source`);
+    console.log('processing, wait ...');
+    const restaurants_michelin = await michelin.scrapeRestaurants(searchLink);
+    const jsonRest = [];
 
-    const restaurant = await michelin.scrapeRestaurant(searchLink);
+    for (let i = 0; i < restaurants_michelin.length;i++) {
+      jsonRest.push(JSON.stringify(restaurants_michelin[i],null,2));
+    }
 
-    console.log(restaurant);
-    console.log('done');
+    if (fs.existsSync('./michelin.json')) {
+      fs.unlinkSync('./michelin.json')
+    }
+
+    fs.appendFileSync('./michelin.json',"[ \n");
+    fs.appendFileSync('./michelin.json',jsonRest);
+    fs.appendFileSync('./michelin.json',"]");
     process.exit(0);
   } catch (e) {
     console.error(e);
@@ -16,6 +29,61 @@ async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-va
   }
 }
 
+
+async function maitre_rest(searchLink = 'https://www.maitresrestaurateurs.fr/annuaire/ajax/loadresult/#') {
+  try {
+    console.log(`browsing ${searchLink} source`);
+    console.log('processing, wait ...');
+    const restaurants_maitre = await maitre.scrapeRestaurantsMaitre(searchLink);
+    console.log(restaurants_maitre);
+    const jsonRest = [];
+    for (let i = 0; i < restaurants_maitre.length;i++) {
+      jsonRest.push(JSON.stringify(restaurants_maitre[i],null,2));
+    }
+
+    if (fs.existsSync('./maitre.json')) {
+      fs.unlinkSync('./maitre.json')
+    }
+
+    fs.appendFileSync('./maitre.json',"[ \n");
+    fs.appendFileSync('./maitre.json',jsonRest);
+    fs.appendFileSync('./maitre.json',"]");
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
 const [,, searchLink] = process.argv;
 
-sandbox(searchLink);
+function merge(){
+  let rawdata_michelin = fs.readFileSync('michelin.json');
+  let rawdata_maitre = fs.readFileSync('maitre.json');
+  let michelin = JSON.parse(rawdata_michelin);
+  let maitre = JSON.parse(rawdata_maitre);
+  const merged = [];
+  for(let i =0;i<michelin.length;i++)
+  {
+    for(let j =0;j<maitre.length;j++){
+      if(michelin[i].name.toLowerCase()==maitre[j].toLowerCase()){
+        merged.push(JSON.stringify(michelin[i],null,2));
+        break;
+      }
+    }
+  }
+  if (fs.existsSync('./merged.json')) {
+    fs.unlinkSync('./merged.json')
+  }
+
+  fs.appendFileSync('./merged.json',"[ \n");
+  fs.appendFileSync('./merged.json',merged);
+  fs.appendFileSync('./merged.json',"]");
+}
+
+//sandbox(searchLink);
+//list_rest_maitre =  maitre_rest(searchLink);
+
+//list_rest_michelin =  michelin_to_json(searchLink);
+
+
+//merge();
